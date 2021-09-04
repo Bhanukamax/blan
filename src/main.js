9ee82e39770e9de29886a99f7bbb6fdaa0db8561
Bhanuka Mallawaarachchi <Bhanukamax@gmail.com>
2021-09-05 00:28:45 +0530

    added example and pretty print ast in compiler mod

--- src/main.js
+++ src/main.js
@@ -1,33 +1,31 @@
 #!/usr/bin/env node
-console.log("test")
+console.log("test");
 const lexer_1 = require("./lexer");
-const parser = require('./parser');
-const repl = require('./repl')
+const parser = require("./parser");
+const repl = require("./repl");
 
-const { readFileSync }  = require('fs');
+const { readFileSync } = require("fs");
 
 function main() {
   // const tokens = lexer.init(input);
   const input = `
 what => this + 2
 `;
-  console.log('the args', process.argv)
-  let fileName = "./test.bmax"
+  console.log("the args", process.argv);
+  let fileName = "./test.bmax";
   if (process.argv.length > 2) {
     fileName = process.argv[2];
 
-  const fileInput = readFileSync(fileName)
-  const lex = new lexer_1.Lexer(fileInput.toString());
-  const tokens = lex.lex();
-  console.log(tokens)
+    const fileInput = readFileSync(fileName);
+    const lex = new lexer_1.Lexer(fileInput.toString());
+    const tokens = lex.lex();
+    console.log(tokens);
 
-  const ast = parser(tokens);
-  console.log('done parsing', ast)
+    const ast = parser(tokens);
+    console.log("done parsing", JSON.stringify(ast, null, 2));
   } else {
     repl();
-    console.log('nothing to be done');
-
+    console.log("nothing to be done");
   }
 }
 main();
-
